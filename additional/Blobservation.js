4339c753aa508e794542bcc45328814645b4d4fc
mikeetree <mikeetree@gmail.com>
2021-06-23 17:00:19 -0400

    WIP: Blobservation

---
+++ additional/Blobservation.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/5abab55b20746bc32e000008
+
+class Blobservation {
+	constructor (h, w) {
+    this.height = h;
+    this.width = w || h;
+  }
+
+  advance_state() {
+
+  }
+
+  move() {
+
+  }
+
+  populate(blobs) {
+
+  }
+
+  print_state() {
+
+  }
+}
